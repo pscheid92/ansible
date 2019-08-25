120555995985219250db43fb519fe2d3c08f9374
Patrick Scheid <p.scheid92@gmail.com>
2019-08-25 19:28:13 +0200

    Install Google Chrome

---
+++ install_google_chrome.sh
@@ -0,0 +1,13 @@
+# Create the repository in etc
+
+cat << EOF > /etc/yum.repos.d/google-chrome.repo
+[google-chrome]
+name=google-chrome
+baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
+enabled=1
+gpgcheck=1
+gpgkey=https://dl.google.com/linux/linux_signing_key.pub
+EOF
+
+# Install with dnf
+dnf install google-chrome-stable
