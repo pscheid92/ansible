46c4851f1abf18189e2016bd10aae3c671e02a6a
Patrick Scheid <p.scheid92@gmail.com>
2019-08-25 20:11:18 +0200

    Install Insync - Fix forgotten change

--- install_insync.sh
+++ install_insync.sh
@@ -5,7 +5,7 @@ rpm --import https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
 cat << 'EOF' > /etc/yum.repos.d/insync.repo
 [insync]
 name=insync repo
-baseurl=http://yum.insync.io/[DISTRIBUTION]/$releasever/
+baseurl=http://yum.insync.io/fedora/$releasever/
 gpgcheck=1
 gpgkey=https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
 enabled=1
