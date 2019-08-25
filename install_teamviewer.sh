b2eb1bd04d0daece816e286421c8884c9b74d574
Patrick Scheid <p.scheid92@gmail.com>
2019-08-25 20:20:15 +0200

    Install TeamViewer

---
+++ install_teamviewer.sh
@@ -0,0 +1,17 @@
+# Import key
+rpm --import http://linux.teamviewer.com/pubkey/TeamViewer2017.asc
+
+# Create repo
+cat << 'EOF' > /etc/yum.repos.d/teamviewer.repo
+[teamviewer]
+name=TeamViewer - $basearch
+baseurl=http://linux.teamviewer.com/yum/stable/main/binary-$basearch/
+gpgkey=http://linux.teamviewer.com/pubkey/TeamViewer2017.asc
+gpgcheck=1
+enabled=1
+type=rpm-md
+failovermethod=priority
+EOF
+
+# Install it
+dnf install teamviewer
