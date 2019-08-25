6c1fbe9e4883182f251db7c09488692719f522dd
Patrick Scheid <p.scheid92@gmail.com>
2019-08-25 20:32:34 +0200

    Install Dropbox

---
+++ install_dropbox.sh
@@ -0,0 +1,13 @@
+# Import key
+rpm --import https://linux.dropbox.com/fedora/rpm-public-key.asc
+
+# Create repo
+cat << 'EOF' > /etc/yum.repos.d/dropbox.repo
+[Dropbox]
+name=Dropbox Repository
+baseurl=http://linux.dropbox.com/fedora/$releasever/
+gpgkey=https://linux.dropbox.com/fedora/rpm-public-key.asc
+EOF
+
+# Install
+dnf install dropbox nautilus-dropbox
