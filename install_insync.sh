ab2aade505f8b69b168c1b1d996c3a200b457fcd
Patrick Scheid <p.scheid92@gmail.com>
2019-08-25 19:47:05 +0200

    Install Insync Google Drive Sync

---
+++ install_insync.sh
@@ -0,0 +1,16 @@
+# Add the key
+rpm --import https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
+
+# Create repo file
+cat << EOF > /etc/yum.repos.d/insync.repo
+[insync]
+name=insync repo
+baseurl=http://yum.insync.io/[DISTRIBUTION]/$releasever/
+gpgcheck=1
+gpgkey=https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
+enabled=1
+metadata_expire=120m
+EOF
+
+# Install insync
+dnf install insync insync-nautilus
