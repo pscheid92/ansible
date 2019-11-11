47d7ac3c6b2ca0b88abca4eddcef2390edc11ebc
Patrick Scheid <p.scheid92@gmail.com>
2019-11-11 20:50:35 +0100

    Install Vertx cli

---
+++ install_vertx.sh
@@ -0,0 +1,9 @@
+# Download vertx distribution to temporary directory and install it
+
+TEMPDIR=$(mktemp -d)
+trap "rm -rf $TEMPDIR" EXIT
+echo "$TEMPDIR"
+
+curl -L -o "$TEMPDIR/vertx.tar.gz" "https://bintray.com/artifact/download/vertx/downloads/vert.x-3.8.3-full.tar.gz"
+tar -C "$TEMPDIR" -zxf "$TEMPDIR/vertx.tar.gz"
+mv "$TEMPDIR/vertx" "/opt/vertx"
