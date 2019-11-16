0b5fc94c546125b7184b3a82c16679d936e89ca1
Patrick Scheid <p.scheid92@gmail.com>
2019-11-16 23:22:08 +0100

    remove podman-docker-replacement and install docker tools

---
+++ install_docker.sh
@@ -0,0 +1,15 @@
+# Install the config-manager plugin to dnf
+dnf install -y dnf-plugins-core
+
+# Add the stable repository
+dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+# Enable the nightly repository
+dnf config-manager --set-enabled docker-ce-nightly
+
+# Install the packages
+dnf install docker-ce docker-ce-cli containerd.io
+
+# Install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
