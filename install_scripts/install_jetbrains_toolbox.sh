a0ddc49f7f2d5f2fe861d827e6cc2a0eae1e10dd
Patrick Scheid <p.scheid92@gmail.com>
2019-12-25 10:07:33 +0100

    Update Jetbrains Toolbox version

--- install_scripts/install_jetbrains_toolbox.sh
+++ install_scripts/install_jetbrains_toolbox.sh
@@ -7,7 +7,7 @@ fi
 
 # Download extract and start
 TMPDIR=`mktemp -d`
-curl -o toolbox.tar.gz https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-1.15.5796.tar.gz
+curl -o toolbox.tar.gz https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.16.6207.tar.gz
 tar -zxf toolbox.tar.gz
 ./jetbrains-toolbox-1.15.5796/jetbrains-toolbox
 rm -rf $TMPDIR
