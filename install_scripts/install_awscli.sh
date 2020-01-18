9809bc52fe10ec356a1494540aec88df4bf28c06
Patrick Scheid <p.scheid92@gmail.com>
2020-01-18 15:17:54 +0100

    Add symbolic links from aws2 to aws

--- install_scripts/install_awscli.sh
+++ install_scripts/install_awscli.sh
@@ -9,3 +9,5 @@ cd $TEMPDIR
 curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 bash aws/install
+ln -s /usr/local/bin/aws2 /usr/local/bin/aws
+ln -s /usr/local/bin/aws2_completer /usr/local/bin/aws_completer
