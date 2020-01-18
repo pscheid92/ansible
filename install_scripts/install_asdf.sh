4f80cade7ab8518f665e4c280d7fae5d4c7e1087
Patrick Scheid <p.scheid92@gmail.com>
2020-01-18 15:14:29 +0100

    Change asdf syntax to new format

--- install_scripts/install_asdf.sh
+++ install_scripts/install_asdf.sh
@@ -5,37 +5,37 @@ ASDF=/home/pscheid/.asdf/bin/asdf
 $ASDF plugin update --all
 
 # Golang
-$ASDF plugin-add golang || true
+$ASDF plugin add golang || true
 $ASDF install golang 1.13.6
 $ASDF global golang 1.13.6
 
 # Gradle
-$ASDF plugin-add gradle || true
+$ASDF plugin add gradle || true
 $ASDF install gradle 6.1
 $ASDF global gradle 6.1
 
 # Java
-$ASDF plugin-add java || true
+$ASDF plugin add java || true
 $ASDF install java azul-zulu-8.42.0.23-jdk8.0.232
 $ASDF install java azul-zulu-13.28.11-jdk13.0.1
 $ASDF global java azul-zulu-13.28.11-jdk13.0.1
 
 # Leiningen
-$ASDF plugin-add lein https://github.com/miorimmax/asdf-lein.git || true
+$ASDF plugin add lein https://github.com/miorimmax/asdf-lein.git || true
 $ASDF install lein 2.9.1
 $ASDF global lein 2.9.1
 
 # Micronaut
-$ASDF plugin-add micronaut || true
+$ASDF plugin add micronaut || true
 $ASDF install micronaut 1.2.9
 $ASDF global micronaut 1.2.9
 
 # NodeJS
-$ASDF plugin-add nodejs || true
+$ASDF plugin add nodejs || true
 $ASDF install nodejs 13.6.0
 $ASDF global nodejs 13.6.0
 
 # SBT
-$ASDF plugin-add sbt || true
+$ASDF plugin add sbt || true
 $ASDF install sbt 1.3.7
 $ASDF global sbt 1.3.7
