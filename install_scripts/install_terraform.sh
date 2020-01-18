bb311d331470478cfb24dc3e5faa375da15abe62
Patrick Scheid <p.scheid92@gmail.com>
2020-01-18 14:56:29 +0100

    Fix: process terraform zip

--- install_scripts/install_terraform.sh
+++ install_scripts/install_terraform.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Create temporary directory
+TEMPDIR=$(mktemp -d)
+trap "rm -rf $TEMPDIR" EXIT
+cd $TEMPDIR
+
+# Configuration
 VERSION="0.12.19"
-URL="https://releases.hashicorp.com/terraform/$VERSION/terraform_$VERSION_linux_amd64.zip"
+URL="https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip"
 TARGET="/usr/local/bin/terraform"
 
-curl -L -o $TARGET $URL
-chmod +x $TARGET
+# Download and extract
+curl -L -o "terraform.zip" "$URL"
+unzip "terraform.zip"
+mv "terraform" $TARGET
+chmod +x "$TARGET"
