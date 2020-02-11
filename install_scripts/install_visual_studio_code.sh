5d10da947b73f6163bb4bf7425844615d2012445
Patrick Scheid <p.scheid92@gmail.com>
2020-02-11 19:24:39 +0100

    Replace Atom by VS Code since keyboard bug is fixed

---
+++ install_scripts/install_visual_studio_code.sh
@@ -0,0 +1,15 @@
+# Import key
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+# Create repository file
+cat << EOF > /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+
+## Install it
+dnf install -y code
