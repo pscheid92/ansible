0a85df497d0b4c64a9a1350d961a1ec29489b03d
Patrick Scheid <p.scheid92@gmail.com>
2020-03-06 19:30:12 +0100

    Update asdf tools

--- install_scripts/install_asdf.sh
+++ install_scripts/install_asdf.sh
@@ -1,52 +1,31 @@
-# Install and configure asdf
+# Configure asdf and Update all plugins
 ASDF=/home/pscheid/.asdf/bin/asdf
-
-# Update all plugins
 $ASDF plugin update --all
 
-# Golang
-$ASDF plugin add golang || true
-$ASDF install golang 1.13.6
-$ASDF global golang 1.13.7
+# ASDF helper
+asdf_install() {
+    $ASDF plugin add "$1"
+    $ASDF install "$1" "$2"
+    $ASDF global "$1" "$2"
+}
 
-# Gradle
-$ASDF plugin add gradle || true
-$ASDF install gradle 6.1.1
-$ASDF global gradle 6.1.1
 
-# Maven
-$ASDF plugin add maven || true
-$ASDF install maven 3.6.3
-$ASDF global maven 3.6.3
+#
+# Install tools
+#
+asdf_install "golang" "1.14"
+asdf_install "gradle" "6.2.2"
+asdf_install "maven" "3.6.3"
+asdf_install "java" "azul-zulu-8.44.0.11-jdk8.0.242"
+asdf_install "java" "azul-zulu-13.29.9-jdk13.0.2"
+asdf_install "clojure" "1.10.1"
+asdf_install "micronaut" "1.3.2"
+asdf_install "nodejs" "13.10.1"
+asdf_install "groovy" "apache-groovy-binary-3.0.1"
 
-# Java
-$ASDF plugin add java || true
-$ASDF install java azul-zulu-8.42.0.23-jdk8.0.232
-$ASDF install java azul-zulu-13.28.11-jdk13.0.1
-$ASDF global java azul-zulu-13.28.11-jdk13.0.1
 
-# Leiningen
+# Leiningen (needs extra installation)
+LEIN_VERSION="2.9.2"
 $ASDF plugin add lein https://github.com/miorimmax/asdf-lein.git || true
-$ASDF install lein 2.9.1
-$ASDF global lein 2.9.1
-
-# Clojure
-$ASDF plugin add clojure
-$ASDF install clojure 1.10.1
-$ASDF global clojure 1.10.1
-
-# Micronaut
-$ASDF plugin add micronaut || true
-$ASDF install micronaut 1.3.0
-$ASDF global micronaut 1.3.0
-
-# NodeJS
-$ASDF plugin add nodejs || true
-$ASDF install nodejs 13.8.0
-$ASDF global nodejs 13.8.0
-
-# Groovy
-$ASDF plugin add groovy
-$ASDF install groovy apache-groovy-binary-3.0.0
-$ASDF global groovy apache-groovy-binary-3.0.0 
-
+$ASDF install lein $LEIN_VERSION
+$ASDF global lein $LEIN_VERSION
