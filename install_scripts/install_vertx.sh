c7701fef60a675299ee7f25c06426c63b2e221ac
Patrick Scheid <p.scheid92@gmail.com>
2020-03-06 19:48:43 +0100

    Update Vert.x to 3.8.5

--- install_scripts/install_vertx.sh
+++ install_scripts/install_vertx.sh
@@ -4,6 +4,6 @@ TEMPDIR=$(mktemp -d)
 trap "rm -rf $TEMPDIR" EXIT
 echo "$TEMPDIR"
 
-curl -L -o "$TEMPDIR/vertx.tar.gz" "https://bintray.com/artifact/download/vertx/downloads/vert.x-3.8.3-full.tar.gz"
+curl -L -o "$TEMPDIR/vertx.tar.gz" "https://bintray.com/artifact/download/vertx/downloads/vert.x-3.8.5-full.tar.gz"
 tar -C "$TEMPDIR" -zxf "$TEMPDIR/vertx.tar.gz"
 mv "$TEMPDIR/vertx" "/opt/vertx"
