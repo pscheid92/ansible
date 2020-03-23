5c5ae382d978fac7c0e9eda92c3366e7d50b64a1
Patrick Scheid <p.scheid92@gmail.com>
2020-03-23 21:58:27 +0100

    Install vault 1.3.4

--- install_scripts/install_asdf.sh
+++ install_scripts/install_asdf.sh
@@ -22,6 +22,7 @@ asdf_install "clojure" "1.10.1"
 asdf_install "micronaut" "1.3.2"
 asdf_install "nodejs" "13.10.1"
 asdf_install "groovy" "apache-groovy-binary-3.0.1"
+asdf_install "vault" "1.3.4"
 
 
 # Leiningen (needs extra installation)
