76f5c0b498e98517968f3c1acd026881fbf80d07
Patrick Scheid <p.scheid92@gmail.com>
2020-11-24 07:58:05 +0100

    remove vertx cli

--- install_scripts/install_vertx.sh
+++
@@ -1,9 +0,0 @@
-# Download vertx distribution to temporary directory and install it
-
-TEMPDIR=$(mktemp -d)
-trap "rm -rf $TEMPDIR" EXIT
-echo "$TEMPDIR"
-
-curl -L -o "$TEMPDIR/vertx.tar.gz" "https://bintray.com/artifact/download/vertx/downloads/vert.x-3.8.5-full.tar.gz"
-tar -C "$TEMPDIR" -zxf "$TEMPDIR/vertx.tar.gz"
-mv "$TEMPDIR/vertx" "/opt/vertx"
