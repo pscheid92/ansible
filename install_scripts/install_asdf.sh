d8ec7b12c6ca76e54390232c183a21379badb205
Patrick Scheid <p.scheid92@gmail.com>
2020-11-24 08:23:27 +0100

    clean and update asdf tools

--- install_scripts/install_asdf.sh
+++ install_scripts/install_asdf.sh
@@ -13,27 +13,13 @@ asdf_install() {
 #
 # Install tools
 #
-asdf_install "golang" "1.14.6"
-asdf_install "gradle" "6.5.1"
+asdf_install "golang" "1.15.5"
+asdf_install "gradle" "6.7.1"
 asdf_install "maven" "3.6.3"
-asdf_install "java" "azul-zulu-8.44.0.11-jdk8.0.242"
-asdf_install "java" "azul-zulu-14.27.1-jdk14"
-asdf_install "clojure" "1.10.1"
-asdf_install "micronaut" "1.3.7"
-asdf_install "nodejs" "14.7.0"
-asdf_install "groovy" "apache-groovy-binary-3.0.5"
-asdf_install "vault" "1.5.0"
-asdf_install "julia" "1.4.2"
-asdf_install "k6" "0.27.1"
-asdf_install "kubectl" "1.18.6"
-asdf_install "minikube" "1.12.1"
-asdf_install "helm" "3.2.4"
-asdf_install "haskell" "8.10.1"
+asdf_install "java" "openjdk-15.0.1"
+asdf_install "nodejs" "15.2.1"
+asdf_install "k6" "0.29.0"
+asdf_install "kubectl" "1.19.4"
+asdf_install "helm" "3.4.1"
+asdf_install "haskell" "8.10.2"
 asdf_install "yq" "3.4.1"
-
-
-# Leiningen (needs extra installation)
-LEIN_VERSION="2.9.4"
-$ASDF plugin add lein https://github.com/miorimmax/asdf-lein.git || true
-$ASDF install lein $LEIN_VERSION
-$ASDF global lein $LEIN_VERSION
